d7bbfc14a6d196606a1a60a106241e068ab80214
Erik <erik@local>
2026-02-01 21:11:47 +0000

    Auto-Save

--- Scripts_Backup/CheckBitLockerStatus.ps1
+++
@@ -1,6 +0,0 @@
-# Check BitLocker Status (Elevated)
-if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Start-Process powershell.exe -Verb RunAs -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`""
-    exit
-}
-manage-bde -status C: | Out-File "$env:USERPROFILE\Documents\BL_Final_Status.txt" -Encoding UTF8
